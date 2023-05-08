176664802030d7dec34bf4443432c0aadb002633
omerfdev <omerfdev@gmail.com>
2023-05-08 09:58:36 +0300

    Commit

--- ButtonGuess/Form1.cs
+++ ButtonGuess/Form1.cs
@@ -48,18 +48,19 @@ namespace ButtonGuess
             button7.Click += Click;
             button8.Click += Click;
             button9.Click += Click;
+
         }
         private void EnabledButton()
         {
-            button1.Enabled = false;
-            button2.Enabled = false;
-            button3.Enabled = false;
-            button4.Enabled = false;
-            button5.Enabled = false;
-            button6.Enabled = false;
-            button7.Enabled = false;
-            button8.Enabled = false;
-            button9.Enabled = false;
+            foreach (var item in this.Controls)
+            {
+                if (item is Button)
+                {
+                    Button btn = item as Button;
+                    btn.Enabled = false;
+                }
+            }
+         
 
         }
    
