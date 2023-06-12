7a4e88337031aa1c543f9bfd64566090dc68473c
omerfdev <omerfdev@gmail.com>
2023-06-12 11:02:07 +0300

    Commit

--- ButtonGuess/Form1.cs
+++ ButtonGuess/Form1.cs
@@ -6,39 +6,39 @@ namespace ButtonGuess
         {
             InitializeComponent();
         }
-        int[] sayilar = new int[9];
-        int sayac = 0;
+        int[] numbers = new int[9];
+        int count = 0;
         int temp = 0;
         private void Form1_Load(object sender, EventArgs e)
         {
             Random rnd = new Random();
 
-            for (int i = 0; i < sayilar.Length; i++)
+            for (int i = 0; i < numbers.Length; i++)
             {
 
-                int sayi = rnd.Next(-4, 5);
-                sayilar[i] = sayi;
+                int number = rnd.Next(-4, 5);
+                numbers[i] = number;
 
-                while (Array.Exists(sayilar, element => element == sayi))
+                while (Array.Exists(numbers, element => element == number))
                 {
-                    sayi = rnd.Next(-4, 5);
+                    number = rnd.Next(-4, 5);
 
                 }
-                sayilar[i] = sayi;
+                numbers[i] = number;
 
 
             }
 
 
-            button1.Tag = sayilar[0];
-            button2.Tag = sayilar[1];
-            button3.Tag = sayilar[2];
-            button4.Tag = sayilar[3];
-            button5.Tag = sayilar[4];
-            button6.Tag = sayilar[5];
-            button7.Tag = sayilar[6];
-            button8.Tag = sayilar[7];
-            button9.Tag = sayilar[8];
+            button1.Tag = numbers[0];
+            button2.Tag = numbers[1];
+            button3.Tag = numbers[2];
+            button4.Tag = numbers[3];
+            button5.Tag = numbers[4];
+            button6.Tag = numbers[5];
+            button7.Tag = numbers[6];
+            button8.Tag = numbers[7];
+            button9.Tag = numbers[8];
             button1.Click += Click;
             button2.Click += Click;
             button3.Click += Click;
@@ -69,7 +69,7 @@ namespace ButtonGuess
             Button btn = (Button)sender;
             btn.Text = btn.Tag.ToString();
             btn.Enabled = false;
-            if (sayac == 0)
+            if (count == 0)
             {
                 if (btn.Text == "0")
                 {
@@ -80,11 +80,11 @@ namespace ButtonGuess
                 else
                 {
                     temp = int.Parse(btn.Text);
-                    sayac++;
+                    count++;
                 }
 
             }
-            else if (sayac == 1)
+            else if (count == 1)
             {
                 temp += int.Parse(btn.Text);
                 if (temp == 0)
